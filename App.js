d8d681cb83c6fe279a3681ec80b201b0e7cca563
giovanafurlan <giovananelofurlan@gmail.com>
2022-08-04 21:26:17 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  Pressable, 
+  StyleSheet,
+  Text,
+  View 
+} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const[result, setResult] = useState();
+
+  function busca(){
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      const foto = data.message;
+      console.log(foto);
+      setResult(foto);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View 
+     style={styles.container}>
+      <Pressable  
+        onPress={busca}
+        style={styles.btn}>
+          <Text style={{
+            color: 'white'
+          }}>
+            Buscar
+          </Text>
+        </Pressable>
+      <img 
+        src={result}
+        width="200"
+        height="200"
+        style={{
+          border: '10px',
+          margin: '50px'
+        }}/>
+   </View>
   );
 }
 
@@ -17,4 +53,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'purple'
+  }
 });
